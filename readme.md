08d3fb1d95e11308f4356b5e0525cc9a64949a52
Russell Matney <russell.matney@gmail.com>
2020-10-22 11:47:49 -0400

    refactor: move to namespaced file, docs for building/running

--- readme.md
+++ readme.md
@@ -2,6 +2,31 @@
 
 An example for working with Babashka to write a simple file watcher.
 
-As written, it exits immediately, but it works great in a repl, because the
-process is kept alive.
+As written, it exits immediately...
 
+But it works great in a repl, where the clojure process is kept alive.
+
+# Usage
+
+## Execute raw
+
+From the root of this project:
+
+```sh
+bb -cp $(clojure -Spath) -m watch.core -- src
+```
+
+## Build and run an uberjar
+
+Pods don't play nicely with the bb uberscript build process,
+but uberjars work just fine.
+
+```sh
+bb -cp $(clojure -Spath) -m watch.core --uberjar watch.jar
+```
+
+Then you can run via:
+
+```sh
+bb watch.jar
+```
--- src/watch/core.clj
+++ src/watch/core.clj
@@ -1,16 +1,16 @@
-(require '[babashka.pods :as pods])
+(ns watch.core
+  (:require [babashka.pods :as pods]))
+
 (pods/load-pod "pod-babashka-filewatcher")
 (require '[pod.babashka.filewatcher :as fw])
 
-(->
-  (Runtime/getRuntime)
-  (.addShutdownHook (Thread. #(println "bye bye!"))))
+(defn -main [& args]
+  (->
+    (Runtime/getRuntime)
+    (.addShutdownHook (Thread. #(println "bye bye!"))))
 
-(let [dir
-      ;; "src"
-      (first *command-line-args*)
-      ]
-  (println "Watching" dir)
-  (fw/watch dir
-            (fn [event] (prn event))
-            {:delay-ms 50}))
+  (let [dir (some-> args first)]
+    (println "Watching" dir)
+    (fw/watch dir
+              (fn [event] (prn event))
+              {:delay-ms 50})))
